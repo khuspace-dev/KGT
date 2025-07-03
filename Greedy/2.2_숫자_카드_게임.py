38edc5adcc15e6196adf8882f8d23a5c481e54c3
DebugSpecialist <kt010724@gmial.com>
2025-07-03 17:43:31 +0900

    dd

--- Greedy/2.2_숫자_카드_게임.py
+++ Greedy/2.2_숫자_카드_게임.py
@@ -1,2 +1,21 @@
 #2.2 숫자 카드 게임
 
+import time
+start_time = time.time()
+
+
+N, M = map(int,input().split())
+card  = []
+
+for i in range(N):
+    line = list(map(int,input().split()))
+    card.append(line)
+
+arr = []
+for i in range(N):
+    arr.append(min(card[i]))
+
+print(max(arr))
+
+end_time = time.time()
+print('time :', end_time - start_time)
